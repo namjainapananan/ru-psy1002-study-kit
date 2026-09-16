ed7a0b329b857b2a313f171c74e5d14cb789a818
namjainapananan <weangprayoonnapanan@gmail.com>
2026-09-16 21:51:42 +0700

    correct registeration

--- registeredquestions.js
+++ registeredquestions.js
@@ -1,9 +1,11 @@
 
-import { ch1Post } from './questions/ch1_post.js';
+import { psy1002_chapter1 } from './questions/psy1002_chapter1.js';
+import { psy1002_chapter2 } from './questions/psy1002_chapter2.js';
 
 // Central list of all available quiz modules
 export const quizRegistry = [
-  ch1Post
+  psy1002_chapter1,
+  psy1002_chapter2
 ];
 
 // Helper to construct a single array containing ALL questions for Marathon Mode
