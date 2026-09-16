12f56ec0f94c3bb050fd2ee50405004439a1d1d1
namjainapananan <weangprayoonnapanan@gmail.com>
2026-09-16 21:48:25 +0700

    chap1 and 2 add

---
+++ questions/template.js
@@ -0,0 +1,12 @@
+
+export const question_id = {
+  id: "question_id",
+  title: "Template",
+  questions: [
+      {
+        question: "Which language provides the raw structure for web pages?",
+        options: ["CSS", "HTML", "JavaScript", "Python"],
+        answer: 1 // Index of correct option ("HTML")
+      },
+  ]
+};
--- registeredquestions.js
+++ registeredquestions.js
@@ -1,10 +1,8 @@
 
-import { ch1Pre } from './questions/ch1_pre.js';
 import { ch1Post } from './questions/ch1_post.js';
 
 // Central list of all available quiz modules
 export const quizRegistry = [
-  ch1Pre,
   ch1Post
 ];
 
