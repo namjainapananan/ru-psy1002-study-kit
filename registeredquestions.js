4bb6329df3a92fae129cfa5c6c6644e34b661626
namjainapananan <weangprayoonnapanan@gmail.com>
2026-09-17 19:29:53 +0700

    corrected var name (stupid ahh mistake)

--- registeredquestions.js
+++ registeredquestions.js
@@ -1,16 +1,16 @@
 
 import { psy1002_chapter1 } from './questions/psy1002_chapter1.js';
 import { psy1002_chapter2 } from './questions/psy1002_chapter2.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter3.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter4.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter5.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter6.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter7.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter8.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter9.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter10.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter11.js';
-import { psy1002_chapter2 } from './questions/psy1002_chapter12.js';
+import { psy1002_chapter3 } from './questions/psy1002_chapter3.js';
+import { psy1002_chapter4 } from './questions/psy1002_chapter4.js';
+import { psy1002_chapter5 } from './questions/psy1002_chapter5.js';
+import { psy1002_chapter6 } from './questions/psy1002_chapter6.js';
+import { psy1002_chapter7 } from './questions/psy1002_chapter7.js';
+import { psy1002_chapter8 } from './questions/psy1002_chapter8.js';
+import { psy1002_chapter9 } from './questions/psy1002_chapter9.js';
+import { psy1002_chapter10 } from './questions/psy1002_chapter10.js';
+import { psy1002_chapter11 } from './questions/psy1002_chapter11.js';
+import { psy1002_chapter12 } from './questions/psy1002_chapter12.js';
 
 // Central list of all available quiz modules
 export const quizRegistry = [
